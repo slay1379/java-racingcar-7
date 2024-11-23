56a1f3ff7369edb24fedaff7cf082bee7ad7b5dd
slay <palgalow@gmail.com>
2024-11-23 15:23:39 +0900

    feat(Benefits): 혜택 내역 클래스 추가

---
+++ src/main/java/christmas/domain/Benefits.java
@@ -0,0 +1,15 @@
+package christmas.domain;
+
+public class Benefits {
+    public int dDayDiscount;
+    public int weekdaysDiscount;
+    public int specialDiscount;
+    public int giftDiscount;
+
+    public Benefits() {
+        this.dDayDiscount = 0;
+        this.weekdaysDiscount = 0;
+        this.specialDiscount = 0;
+        this.giftDiscount = 0;
+    }
+}
