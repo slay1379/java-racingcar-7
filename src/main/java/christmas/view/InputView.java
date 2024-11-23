d581447b0b899fd64261a3fd42b2782e97366777
slay <palgalow@gmail.com>
2024-11-23 14:23:07 +0900

    refactor(InputView): 주문 메뉴 입력 기능 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,11 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_VISIT_DAY_MSG = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String INPUT_ORDER_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
     public int readVisitDay() {
         System.out.println(INPUT_VISIT_DAY_MSG);
         return Integer.parseInt(Console.readLine());
     }
 
-
+    public String readOrderMenu() {
+        System.out.println(INPUT_ORDER_MENU);
+        return Console.readLine();
+    }
 }
