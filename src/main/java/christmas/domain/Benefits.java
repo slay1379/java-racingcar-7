4be66853bd2e3d81176736afafbf42196d6a6568
slay <palgalow@gmail.com>
2024-11-23 16:33:27 +0900

    feat(Benefits): 주말 할인 변수 추가

--- src/main/java/christmas/domain/Benefits.java
+++ src/main/java/christmas/domain/Benefits.java
@@ -3,12 +3,14 @@ package christmas.domain;
 public class Benefits {
     public int dDayDiscount;
     public int weekdaysDiscount;
+    public int weekendDiscount;
     public int specialDiscount;
     public int giftDiscount;
 
     public Benefits() {
         this.dDayDiscount = 0;
         this.weekdaysDiscount = 0;
+        this.weekendDiscount = 0;
         this.specialDiscount = 0;
         this.giftDiscount = 0;
     }
