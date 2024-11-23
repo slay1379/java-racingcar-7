76060746d704780f6467d8be0448bc330ace65ba
slay <palgalow@gmail.com>
2024-11-23 15:52:14 +0900

    feat(BenefitService): 평일인지 검증하는 기능 추가

--- src/main/java/christmas/service/BenefitService.java
+++ src/main/java/christmas/service/BenefitService.java
@@ -1,23 +1,40 @@
 package christmas.service;
 
 import christmas.domain.Benefits;
+import christmas.domain.OrderMenu;
+import java.util.List;
 
 public class BenefitService {
 
     private Benefits benefits;
+    private List<OrderMenu> orderMenus;
+    private int day;
 
-    public BenefitService(Benefits benefits) {
+    public BenefitService(Benefits benefits, List<OrderMenu> orderMenus, int day) {
         this.benefits = benefits;
+        this.orderMenus = orderMenus;
+        this.day = day;
     }
 
-    public void calculateDDayDiscount(int day) {
+    public void calculateDDayDiscount() {
         if (isPeriodChristmasDiscount(day)) {
             benefits.dDayDiscount = day * 100;
         }
     }
 
-    private boolean isPeriodChristmasDiscount(int day) {
+    public void calculateWeekdaysDiscount() {
+
+    }
+
+    private boolean isPeriodChristmasDiscount() {
         if (day > 25) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isWeeksday() {
+        if (day % 7 == 1 || day % 7 == 2) {
             return false;
         }
         return true;
