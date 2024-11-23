4dbeccd07946651f4212f887364414e4b4fc7379
slay <palgalow@gmail.com>
2024-11-23 14:20:31 +0900

    feat(InputView): 예상 방문 날짜 입력 기능 추가

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,12 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String INPUT_VISIT_DAY_MSG = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+
+    public String readVisitDay() {
+        System.out.println(INPUT_VISIT_DAY_MSG);
+        return Console.readLine();
+    }
+}
