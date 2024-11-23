e1f7f26fe9fbf9ea203db3ad2108b271ae1a390b
slay <palgalow@gmail.com>
2024-11-23 15:17:42 +0900

    feat(OutputView): 할인 전 총주문 금액 출력 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -6,6 +6,13 @@ import java.util.List;
 public class OutputView {
     private static final String WELCOME_MSG = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String ORDER_MENUS = "<주문 메뉴>";
+    private static final String TOTAL_PRICE_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
+    private static final String GIFT_MENU = "<증정 메뉴>";
+    private static final String BENEFIT_DETAILS = "<혜택 내역>";
+    private static final String TOTAL_BENEFIT_PRICE = "<총혜택 금액>";
+    private static final String TOTAL_PRICE_AFTER_DISCOUNT = "<할인 후 예상 결제 금액>";
+    private static final String EVENT_BADGE = "<12월 이벤트 배지>";
+
 
     public void printWelcomeMsg() {
         System.out.println(WELCOME_MSG);
@@ -17,4 +24,11 @@ public class OutputView {
             System.out.println(orderMenu.name+" "+orderMenu.quantity+"개");
         }
     }
+
+    public void printTotalPriceBeforeDiscount(int totalPriceBeforeDiscount) {
+        System.out.println(TOTAL_PRICE_BEFORE_DISCOUNT);
+        System.out.println(totalPriceBeforeDiscount+"원");
+    }
+
+
 }
