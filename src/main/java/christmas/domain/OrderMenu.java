f17262c50b5798c3b8c9c0f5e286b5924496d9e3
slay <palgalow@gmail.com>
2024-11-23 14:28:16 +0900

    feat(OrderMenu): 주문메뉴 객체 생성

---
+++ src/main/java/christmas/domain/OrderMenu.java
@@ -0,0 +1,11 @@
+package christmas.domain;
+
+public class OrderMenu {
+    public String name;
+    public int quantity;
+
+    public OrderMenu(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
