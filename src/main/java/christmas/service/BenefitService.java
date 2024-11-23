5538b4e05a8d4f42071c9be527b4a2d6a628f7a5
slay <palgalow@gmail.com>
2024-11-23 16:30:04 +0900

    feat(BenefitService): 평일 할인 계산 기능 추가

--- src/main/java/christmas/service/BenefitService.java
+++ src/main/java/christmas/service/BenefitService.java
@@ -1,29 +1,43 @@
 package christmas.service;
 
 import christmas.domain.Benefits;
+import christmas.domain.MenuType;
+import christmas.domain.Menus;
 import christmas.domain.OrderMenu;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BenefitService {
+
+    private static final int DESSERT_DISCOUNT_AMOUNT = 2023;
 
     private Benefits benefits;
     private List<OrderMenu> orderMenus;
     private int day;
+    private Menus menus;
 
-    public BenefitService(Benefits benefits, List<OrderMenu> orderMenus, int day) {
+    public BenefitService(Benefits benefits, List<OrderMenu> orderMenus, int day, Menus menus) {
         this.benefits = benefits;
         this.orderMenus = orderMenus;
         this.day = day;
+        this.menus = menus;
     }
 
     public void calculateDDayDiscount() {
-        if (isPeriodChristmasDiscount(day)) {
+        if (isPeriodChristmasDiscount()) {
             benefits.dDayDiscount = day * 100;
         }
     }
 
     public void calculateWeekdaysDiscount() {
-
+        if (isWeeksday()) {
+            for (OrderMenu orderMenu : orderMenus) {
+                if (menus.findMenuTypeByName(orderMenu.name) == MenuType.DESSERT) {
+                    benefits.weekdaysDiscount += DESSERT_DISCOUNT_AMOUNT;
+                }
+            }
+        }
     }
 
     private boolean isPeriodChristmasDiscount() {
