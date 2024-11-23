1a567b9b17a1851bd045223cd1bed778ad17c70b
slay <palgalow@gmail.com>
2024-11-23 15:09:52 +0900

    feat(InputView): 입력 받은 주문 객체리스트로 파싱 기능 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,6 +1,9 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.OrderMenu;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
     private static final String INPUT_VISIT_DAY_MSG = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
@@ -11,8 +14,22 @@ public class InputView {
         return Integer.parseInt(Console.readLine());
     }
 
-    public String readOrderMenu() {
+    public List<OrderMenu> readOrderMenu() {
         System.out.println(INPUT_ORDER_MENU);
-        return Console.readLine();
+        String[] orderMenus = splitOrderMenu(Console.readLine());
+        return parseOrderMenus(orderMenus);
+    }
+
+    private String[] splitOrderMenu(String orderMenu) {
+        return orderMenu.split(",");
+    }
+
+    private List<OrderMenu> parseOrderMenus(String[] orderMenus) {
+        List<OrderMenu> orderMenuList = new ArrayList<>();
+        for (String orderMenu : orderMenus) {
+            String[] split = orderMenu.split("-");
+            orderMenuList.add(new OrderMenu(split[0], Integer.parseInt(split[1])));
+        }
+        return orderMenuList;
     }
 }
